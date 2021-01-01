56cca8c533bcab2e15e53844c2418e397d8d4069
CarlosNeto2804 <carloshtcursos@gmail.com>
2021-01-01 16:40:59 -0400

    Compreencao de Listas

---
+++ 2-modulo-listas/compreencao-lista.hs
@@ -0,0 +1,23 @@
+listaUmADez :: [Int]
+listaUmADez = [1 .. 10]
+
+
+listaMultiplicadaPorDois :: [Int]
+listaMultiplicadaPorDois = [x * 2 | x <- [1 .. 10]]
+
+listaPares :: [Int] 
+listaPares = [x | x <- [1 .. 10],  even x]
+
+
+ePar :: Int -> Bool 
+ePar n = mod n 2 == 0 --ou even 2
+
+eMaiorQueCinco :: Int -> Bool 
+eMaiorQueCinco n = n > 5
+
+listaDeParesMaioresQueCinco :: [Int]
+listaDeParesMaioresQueCinco = [x | x <- [1 ..10], ePar x, eMaiorQueCinco x] 
+
+
+listaDeTuplas :: [(Int, Int)]
+listaDeTuplas = [(x,y) | x <- [1..5], y <- [20..25]]
