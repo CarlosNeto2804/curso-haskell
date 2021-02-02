7124eaa3e7036e19ef0bc2ddd446364ec6c866a4
CarlosNeto2804 <carloshtcursos@gmail.com>
2021-02-02 19:45:16 -0400

    exercicio em listas

--- 2-modulo-listas/exercicios-lista.hs
+++ 2-modulo-listas/exercicios-lista.hs
@@ -17,3 +17,15 @@ todosInteirosSaoPares [] = True
 todosInteirosSaoPares (cabecaLista:corpoLista) 
     | mod cabecaLista 2  /= 0 = False -- forma 1 pode ser substuituido por odd
     | otherwise  = todosInteirosSaoPares corpoLista
+
+-- concatenar duas listas
+concatenarListasGenerico :: [a] -> [a]-> [a]
+concatenarListasGenerico [] y = y
+concatenarListasGenerico x [] = x
+concatenarListasGenerico (x:xs) y =
+    x : concatenarListasGenerico xs y
+
+--inverter lista
+inverterLista :: [a] -> [a]
+inverterLista [] = []
+inverterLista (x:xs) = inverterLista xs ++ [x]
